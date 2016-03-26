9434a624365a979c9db30914a10a66409a83835d
davidgilman <daviidgilman@gmail.com>
2016-03-25 23:52:49 -0700

    first commit

---
+++ Estarea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Estarea
+//
+//  Created by David Gilman on 3/25/16.
+//  Copyright © 2016 David Gilman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
